3ede3794a9d698ff2cdd1a99ba30a69aa62e454d
jakesungjaekim <codejakecarry@gmail.com>
2022-05-26 14:50:08 +0900

    working - footer [100%]

--- src/components/Netflix/NeflixQuestion.js
+++ src/components/Netflix/NeflixQuestion.js
@@ -3,34 +3,35 @@ import { Fragment } from "react";
 
 import classes from "./NeflixQuestion.module.css";
 
+import footerImg from "../../assets/images/footer-plusimg.png";
+
+const DUMMY_QUESTIONS = [
+  { id: 1, name: "넷플릭스란 무엇인가요?" },
+  { id: 2, name: "넷플릭스 요금은 얼마인가요?" },
+  { id: 3, name: "어디에서 시청할 수 있나요?" },
+  { id: 4, name: "멤버쉽을 해지하려면 어떻게 하나요?" },
+  { id: 5, name: "넷플릭스에서 어떤 콘텐츠를 시청할 수 있나요?" },
+  { id: 6, name: "아이들이 넷플릭스를 봐도 좋을까요?" },
+];
 const NeflixQuestion = (props) => {
+  const questionList = DUMMY_QUESTIONS.map((question) => {
+    return (
+      <div className={classes["questions-wrap"]} key={question.id}>
+        {question.name}
+        <div>
+          <img src={footerImg} />
+        </div>
+      </div>
+    );
+  });
+
   return (
     <Fragment>
-      <form className={classes.question}>
-        <form>
+      <div className={classes.question}>
+        <div>
           <h2>자주 묻는 질문</h2>
 
-          {/* <div class="buttons">
-            <button class="q-button">넷플릭스란 무엇인가요?
-            <img src="../images/plus.png">
-            </button>
-            <button class="q-button">넷플릭스 요금은 얼마인가요?
-            <img src="../images/plus.png">
-            </button>
-            <button class="q-button">어디에서 시청할 수 있나요?
-            <img src="../images/plus.png">
-            </button>
-            <button class="q-button">멤버쉽을 해지하려면 어떻게 하나요?
-            <img src="../images/plus.png">
-            </button>
-            <button class="q-button">
-            넷플릭스에서 어떤 콘텐츠를 시청할 수 있나요?
-            <img src="../images/plus.png">
-            </button>
-            <button class="q-button">아이들이 넷플릭스를 봐도 좋을까요?
-            <img src="../images/plus.png">
-            </button>
-          </div> */}
+          <div className={classes.questionslist}>{questionList}</div>
 
           <div className={classes.onemoretime}>
             <p>
@@ -42,8 +43,8 @@ const NeflixQuestion = (props) => {
               <button>시작하기</button>
             </div>
           </div>
-        </form>
-      </form>
+        </div>
+      </div>
     </Fragment>
   );
 };
