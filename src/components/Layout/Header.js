506e3c6adecd37be959cfa500e9389c63b076929
jakesungjaekim <codejakecarry@gmail.com>
2022-06-01 14:21:45 +0900

    Update Link to Page

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,15 +9,19 @@ import classes from "./Header.module.css";
 import HeaderLangOption from "./HeaderLangOption";
 import HeaderLoginButton from "./HeaderLoginButton";
 
+import { Link } from "react-router-dom";
+
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <img
-          className={classes["header-image"]}
-          src={netflixImg}
-          alt="Netflix-logo"
-        />
+        <Link to="/">
+          <img
+            className={classes["header-image"]}
+            src={netflixImg}
+            alt="Netflix-logo"
+          />
+        </Link>
         <div className={classes["header-side"]}>
           <HeaderLangOption />
           <HeaderLoginButton />
--- src/components/Layout/HeaderLoginButton.js
+++ src/components/Layout/HeaderLoginButton.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Fragment } from "react";
 
 import classes from "./HeaderLoginButton.module.css";
+import { Link } from "react-router-dom";
 
 const HeaderLoginButton = (props) => {
   return (
     <Fragment>
-      <button className={classes.button}>로그인</button>
+      <Link to="/login">
+        <button className={classes.button}>로그인</button>
+      </Link>
     </Fragment>
   );
 };
